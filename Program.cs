464a423c63148ee0fec4df6106f30047d0c0083c
Lyudmila <lyudmila150983@gmail.com>
2023-01-05 23:26:49 +0300

    вводим массив

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу которая из имеющегося массива строк формирует массив из строк, 
+//длинна которых меньше либо равна 3 символам.
+
+ string[] M;
+            Console.WriteLine("Введите массив строк через пробел");
+            string words = Console.ReadLine();
+            M = words.Split(' ');
+            
+              
+            var Long= new string[M .Length];
+            var Count = 0;
+            foreach (var temp in M )
+            {
+                if (temp.Length <= 3)
+                {
+                    Long[Count] = temp;
+                    Count++;
+                    Console.Write($"'{temp}',"); 
+                }
+               
+            }
+            
+            // Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            //Console.ReadKey(true);
